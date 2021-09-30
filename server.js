f73f38ebaf2985ee254b83e6b7b6e148111707eb
Aaron Spurlock <Aaron.Spurlock@cslico.com>
2021-09-30 15:10:56 -0400

    fixed db.once

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', error => console.log('Connected to MongoDB!'))
+db.once('open', () => console.log('Connected to MongoDB!'))
 
 
 app.listen(process.env.PORT || 3000)
